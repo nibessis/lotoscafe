35953971cce5841c36707596f2b48dbdb65bbdff
nibessis <Nibessis1313@gmail.com>
2025-08-26 14:01:37 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // --- bump this on every deploy ---
-const CACHE_VERSION = 'v14';
+const CACHE_VERSION = 'v15';
 const STATIC_CACHE = `lotos-cafe-static-${CACHE_VERSION}`;
 
 // Precache only the core shell. Big images are optional.
@@ -84,6 +84,7 @@ async function staleWhileRevalidate(req) {
     .catch(() => cached);
   return cached || fetchPromise;
 }
+
 
 
 
