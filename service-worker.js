5c32609b80aa2c40b142f5772c4f82b7970e6cac
nibessis <Nibessis1313@gmail.com>
2026-04-07 07:41:43 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // --- bump this on every deploy ---
-const CACHE_VERSION = 'v22';
+const CACHE_VERSION = 'v23';
 const STATIC_CACHE = `lotos-cafe-static-${CACHE_VERSION}`;
 
 // Precache only the core shell. Big images are optional.
